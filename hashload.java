feb64b163741d71879b21271e736ce2fb3b195fb
rmit-s3607187-chunhang-chan <s3607187@student.rmit.edu.au>
2018-05-15 23:48:11 +1000

    Update hashload.java

--- hashload.java
+++ hashload.java
@@ -1,42 +1,39 @@
 import java.io.*;
+import java.util.*;
 import java.nio.ByteBuffer;
 
 /**
  *  Database Systems - HEAP IMPLEMENTATION
  */
 
-public class hashload implements dbimpl
+public class dbload implements dbimpl
 {
-
-
-
-   // initialize
+    // initialize
    public static void main(String args[])
    {
-      hashload load = new hashload();
+      dbload load = new dbload();
 
-      // calculate query time
+      // calculate load time
       long startTime = System.currentTimeMillis();
       load.readArguments(args);
       long endTime = System.currentTimeMillis();
 
-      System.out.println("Query time: " + (endTime - startTime) + "ms");
+      System.out.println("Load time: " + (endTime - startTime) + "ms");
    }
-
 
    // reading command line arguments
    public void readArguments(String args[])
    {
-      if (args.length == 2)
+      if (args.length == 3)
       {
-         if (isInteger(args[1]))
+         if (args[0].equals("-p") && isInteger(args[1]))
          {
-            readHeap(args[0], Integer.parseInt(args[1]));
+            readFile(args[2], Integer.parseInt(args[1]));
          }
       }
       else
       {
-          System.out.println("Error: only pass in two arguments");
+         System.out.println("Error: only pass in three arguments");
       }
    }
 
@@ -56,104 +53,137 @@ public class hashload implements dbimpl
       return isValidInt;
    }
 
-   // read heapfile by page
-   public void readHeap(String name, int pagesize)
+   // read .csv file using buffered reader
+   public void readFile(String filename, int pagesize)
    {
+      dbload load = new dbload();
       File heapfile = new File(HEAP_FNAME + pagesize);
-      int intSize = 4;
-      int pageCount = 0;
-      int recCount = 0;
-      int recordLen = 0;
-      int rid = 0;
-      boolean isNextPage = true;
-      boolean isNextRecord = true;
+      BufferedReader br = null;
+      FileOutputStream fos = null;
+      String line = "";
+      String nextLine = "";
+      String stringDelimeter = "\t";
+      byte[] RECORD = new byte[RECORD_SIZE];
+      int outCount, pageCount, recCount;
+      outCount = pageCount = recCount = 0;
+
       try
       {
-         FileInputStream fis = new FileInputStream(heapfile);
-         // reading page by page
-         while (isNextPage)
+         // create stream to write bytes to according page size
+         fos = new FileOutputStream(heapfile);
+         br = new BufferedReader(new FileReader(filename));
+         // read line by line
+         while ((line = br.readLine()) != null)
          {
-            byte[] bPage = new byte[pagesize];
-            byte[] bPageNum = new byte[intSize];
-            fis.read(bPage, 0, pagesize);
-            System.arraycopy(bPage, bPage.length-intSize, bPageNum, 0, intSize);
-
-            // reading by record, return true to read the next record
-            isNextRecord = true;
-            while (isNextRecord)
+            String[] entry = line.split(stringDelimeter, -1);
+            RECORD = createRecord(RECORD, entry, outCount);
+            // outCount is to count record and reset everytime
+            // the number of bytes has exceed the pagesize
+            outCount++;
+            fos.write(RECORD);
+            if ((outCount+1)*RECORD_SIZE > pagesize)
             {
-               byte[] bRecord = new byte[RECORD_SIZE];
-               byte[] bRid = new byte[intSize];
-               try
-               {
-                  System.arraycopy(bPage, recordLen, bRecord, 0, RECORD_SIZE);
-                  System.arraycopy(bRecord, 0, bRid, 0, intSize);
-                  rid = ByteBuffer.wrap(bRid).getInt();
-                  if (rid != recCount)
-                  {
-                     isNextRecord = false;
-                  }
-                  else
-                  {
-                     printRecord(bRecord, name);
-                     recordLen += RECORD_SIZE;
-                  }
-                  recCount++;
-                  // if recordLen exceeds pagesize, catch this to reset to next page
-               }
-               catch (ArrayIndexOutOfBoundsException e)
-               {
-                  isNextRecord = false;
-                  recordLen = 0;
-                  recCount = 0;
-                  rid = 0;
-               }
+               eofByteAddOn(fos, pagesize, outCount, pageCount);
+               //reset counter to start newpage
+               outCount = 0;
+               pageCount++;
             }
-            // check to complete all pages
-            if (ByteBuffer.wrap(bPageNum).getInt() != pageCount)
-            {
-               isNextPage = false;
-            }
-            pageCount++;
+            recCount++;
          }
       }
       catch (FileNotFoundException e)
       {
-         System.out.println("File: " + HEAP_FNAME + pagesize + " not found.");
+         System.out.println("File: " + filename + " not found.");
       }
-      catch (IOException e)
+      catch (Exception e)
       {
          e.printStackTrace();
       }
+      finally
+      {
+         if (br != null)
+         {
+            try
+            {
+               // final add on at end of file
+               if ((nextLine = br.readLine()) == null)
+               {
+                  eofByteAddOn(fos, pagesize, outCount, pageCount);
+                  pageCount++;
+               }
+               fos.close();
+               br.close();
+            }
+            catch (IOException e)
+            {
+               e.printStackTrace();
+            }
+         }
+      }
+      System.out.println("Page total: " + pageCount);
+      System.out.println("Record total: " + recCount);
    }
 
-   // returns records containing the argument text from shell
-   public void printRecord(byte[] rec, String input)
+   // create byte array for a field and append to record array at correct 
+   // offset using array copy
+   public void copy(String entry, int SIZE, int DATA_OFFSET, byte[] rec)
+          throws UnsupportedEncodingException
    {
-
-      try{
-         RandomAccessFile raf = new RandomAccessFile("test.txt", "rw");
-         String record = new String(rec);
-         String BN_NAME = record
-                 .substring(RID_SIZE+REGISTER_NAME_SIZE,
-                         RID_SIZE+REGISTER_NAME_SIZE+BN_NAME_SIZE);
-         //if (BN_NAME.toLowerCase().contains(input.toLowerCase()))
-         //{
-         //  System.out.println(BN_NAME);
-         //}
-
-         int  bucket = (BN_NAME.hashCode() & 0xfffffff) % 4000000;         // creates a hash and keeps it between 0 - 4m
-         byte  BUCKET_SIZE = (byte) 300;                    // size of static bucket
-         int  offset = bucket * BUCKET_SIZE;             // offset.
-
-         raf.seek(offset);                // goes to the offset
-         raf.writeBytes(BN_NAME);      // writes the String into the particular spot.
-
-
-
-      }catch(IOException ex){
-
+      byte[] DATA = new byte[SIZE];
+      byte[] DATA_SRC = entry.trim().getBytes(ENCODING);
+      if (entry != "")
+      {
+         System.arraycopy(DATA_SRC, 0,
+                DATA, 0, DATA_SRC.length);
       }
+      System.arraycopy(DATA, 0, rec, DATA_OFFSET, DATA.length);
+   }
 
+   // creates record by appending using array copy and then applying offset
+   // where neccessary
+   public byte[] createRecord(byte[] rec, String[] entry, int out)
+          throws UnsupportedEncodingException 
+   {
+      byte[] RID = intToByteArray(out);
+      System.arraycopy(RID, 0, rec, 0, RID.length);
+
+      copy(entry[0], REGISTER_NAME_SIZE, RID_SIZE, rec);
+
+      copy(entry[1], BN_NAME_SIZE, BN_NAME_OFFSET, rec);
+
+      copy(entry[2], BN_STATUS_SIZE, BN_STATUS_OFFSET, rec);
+
+      copy(entry[3], BN_REG_DT_SIZE, BN_REG_DT_OFFSET, rec);
+
+      copy(entry[4], BN_CANCEL_DT_SIZE, BN_CANCEL_DT_OFFSET, rec);
+
+      copy(entry[5], BN_RENEW_DT_SIZE, BN_RENEW_DT_OFFSET, rec);
+
+      copy(entry[6], BN_STATE_NUM_SIZE, BN_STATE_NUM_OFFSET, rec);
+
+      copy(entry[7], BN_STATE_OF_REG_SIZE, BN_STATE_OF_REG_OFFSET, rec);
+
+      copy(entry[8], BN_ABN_SIZE, BN_ABN_OFFSET, rec);
+
+      return rec;
+   }
+
+   // EOF padding to fill up remaining pagesize
+   // * minus 4 bytes to add page number at end of file
+   public void eofByteAddOn(FileOutputStream fos, int pSize, int out, int pCount) 
+          throws IOException
+   {
+      byte[] fPadding = new byte[pSize-(RECORD_SIZE*out)-4];
+      byte[] bPageNum = intToByteArray(pCount);
+      fos.write(fPadding);
+      fos.write(bPageNum);
+   }
+
+   // converts ints to a byte array of allocated size using bytebuffer
+   public byte[] intToByteArray(int i)
+   {
+      ByteBuffer bBuffer = ByteBuffer.allocate(4);
+      bBuffer.putInt(i);
+      return bBuffer.array();
    }
 }
