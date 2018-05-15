dff7c9a0f6fa8a4a557dbbcdcdb5b32146857a4d
rmit-s3607187-chunhang-chan <s3607187@student.rmit.edu.au>
2018-05-15 23:35:33 +1000

    Add files via upload

---
+++ hashload.java
@@ -0,0 +1,159 @@
+import java.io.*;
+import java.nio.ByteBuffer;
+
+/**
+ *  Database Systems - HEAP IMPLEMENTATION
+ */
+
+public class hashload implements dbimpl
+{
+
+
+
+   // initialize
+   public static void main(String args[])
+   {
+      hashload load = new hashload();
+
+      // calculate query time
+      long startTime = System.currentTimeMillis();
+      load.readArguments(args);
+      long endTime = System.currentTimeMillis();
+
+      System.out.println("Query time: " + (endTime - startTime) + "ms");
+   }
+
+
+   // reading command line arguments
+   public void readArguments(String args[])
+   {
+      if (args.length == 2)
+      {
+         if (isInteger(args[1]))
+         {
+            readHeap(args[0], Integer.parseInt(args[1]));
+         }
+      }
+      else
+      {
+          System.out.println("Error: only pass in two arguments");
+      }
+   }
+
+   // check if pagesize is a valid integer
+   public boolean isInteger(String s)
+   {
+      boolean isValidInt = false;
+      try
+      {
+         Integer.parseInt(s);
+         isValidInt = true;
+      }
+      catch (NumberFormatException e)
+      {
+         e.printStackTrace();
+      }
+      return isValidInt;
+   }
+
+   // read heapfile by page
+   public void readHeap(String name, int pagesize)
+   {
+      File heapfile = new File(HEAP_FNAME + pagesize);
+      int intSize = 4;
+      int pageCount = 0;
+      int recCount = 0;
+      int recordLen = 0;
+      int rid = 0;
+      boolean isNextPage = true;
+      boolean isNextRecord = true;
+      try
+      {
+         FileInputStream fis = new FileInputStream(heapfile);
+         // reading page by page
+         while (isNextPage)
+         {
+            byte[] bPage = new byte[pagesize];
+            byte[] bPageNum = new byte[intSize];
+            fis.read(bPage, 0, pagesize);
+            System.arraycopy(bPage, bPage.length-intSize, bPageNum, 0, intSize);
+
+            // reading by record, return true to read the next record
+            isNextRecord = true;
+            while (isNextRecord)
+            {
+               byte[] bRecord = new byte[RECORD_SIZE];
+               byte[] bRid = new byte[intSize];
+               try
+               {
+                  System.arraycopy(bPage, recordLen, bRecord, 0, RECORD_SIZE);
+                  System.arraycopy(bRecord, 0, bRid, 0, intSize);
+                  rid = ByteBuffer.wrap(bRid).getInt();
+                  if (rid != recCount)
+                  {
+                     isNextRecord = false;
+                  }
+                  else
+                  {
+                     printRecord(bRecord, name);
+                     recordLen += RECORD_SIZE;
+                  }
+                  recCount++;
+                  // if recordLen exceeds pagesize, catch this to reset to next page
+               }
+               catch (ArrayIndexOutOfBoundsException e)
+               {
+                  isNextRecord = false;
+                  recordLen = 0;
+                  recCount = 0;
+                  rid = 0;
+               }
+            }
+            // check to complete all pages
+            if (ByteBuffer.wrap(bPageNum).getInt() != pageCount)
+            {
+               isNextPage = false;
+            }
+            pageCount++;
+         }
+      }
+      catch (FileNotFoundException e)
+      {
+         System.out.println("File: " + HEAP_FNAME + pagesize + " not found.");
+      }
+      catch (IOException e)
+      {
+         e.printStackTrace();
+      }
+   }
+
+   // returns records containing the argument text from shell
+   public void printRecord(byte[] rec, String input)
+   {
+
+      try{
+         RandomAccessFile raf = new RandomAccessFile("test.txt", "rw");
+         String record = new String(rec);
+         String BN_NAME = record
+                 .substring(RID_SIZE+REGISTER_NAME_SIZE,
+                         RID_SIZE+REGISTER_NAME_SIZE+BN_NAME_SIZE);
+         //if (BN_NAME.toLowerCase().contains(input.toLowerCase()))
+         //{
+         //  System.out.println(BN_NAME);
+         //}
+
+         int  bucket = (BN_NAME.hashCode() & 0xfffffff) % 4000000;         // creates a hash and keeps it between 0 - 4m
+         byte  BUCKET_SIZE = (byte) 300;                    // size of static bucket
+         int  offset = bucket * BUCKET_SIZE;             // offset.
+
+         raf.seek(offset);                // goes to the offset
+         raf.writeBytes(BN_NAME);      // writes the String into the particular spot.
+
+
+
+      }catch(IOException ex){
+
+      }
+
+   }
+}
---
+++ hashquery.java
@@ -0,0 +1,73 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class hashquery {
+
+
+
+    // initialize
+    public static void main(String args[])
+    {
+
+        // calculate query time
+        long startTime = System.currentTimeMillis();
+
+        readHash(args[0]);
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Query time: " + (endTime - startTime) + "ms");
+    }
+
+
+    // reading command line arguments
+    public static void readArguments(String args[])
+    {
+        if (args.length == 2)
+        {
+            readHash(args[0]);
+
+        }
+        else
+        {
+            System.out.println("Error: only pass in two arguments");
+        }
+    }
+
+    public static void readHash(String x){
+
+
+
+        try{
+            RandomAccessFile raf = new RandomAccessFile("test.txt", "r");
+
+            int bucket = (x.hashCode() & 0xfffffff) % 4000000;
+            byte BUCKET_SIZE = (byte) 300;
+            int offset = bucket * BUCKET_SIZE;
+
+
+            //if (BN_NAME.toLowerCase().contains(input.toLowerCase()))
+            //{
+            //  System.out.println(BN_NAME);
+            //}
+
+	
+
+            raf.seek(offset);
+            System.out.println(raf.read());
+
+
+
+        }catch(IOException ex){
+
+        }
+
+
+
+
+
+
+    }
+
+
+}
